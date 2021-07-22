698103b0e76815919735733d1c3351c9de15913a
Jared Long <jlong4223@gmail.com>
2021-07-22 17:10:37 -0400

    fix: updates pw validation and adds relationship to details

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :details
     
     validates :name, presence: true
     validates :email, presence: true, :uniqueness => {:case_sensitive => false}
-    validates :password, presence: true, length: { minimum: 4 }
-
+    # `:if => :password` - skips checking if the password is not being set; 
+    # allows easier patch/put of user information w/out needing password for req
+    validates :password, presence: true, length: { minimum: 4 }, :if => :password
 end
