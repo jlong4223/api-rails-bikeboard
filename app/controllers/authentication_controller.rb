58ea11a223301fa624c444943e5149fbe73302ad
Jared Long <jlong4223@gmail.com>
2021-09-07 15:39:21 -0500

    feat: comments and updates details id

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,6 +3,10 @@ class AuthenticationController < ApplicationController
         # finding user by email
         user = User.find_by(email: params[:email])
         
+        # puts "user has details: #{user.details[0] ? true : false}"
+
+        # TODO watch this to see what happens when a user does add details
+
         if user && user.authenticate(params[:password])
         # encoding the token with id, name, email, role, details_id
          auth_token = JsonWebToken.encode(
@@ -11,7 +15,7 @@ class AuthenticationController < ApplicationController
                     name: user.name, 
                     email: user.email,
                     role: user.role,
-                    details_id: user.details[0].id
+                    details_id: user.details[0] ? user.details[0].id : nil
                 }
             )
             
