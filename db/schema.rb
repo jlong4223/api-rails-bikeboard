d6c7425c9f964b89147fe09f5b518dbf44c9530c
Jared Long <jlong4223@gmail.com>
2021-09-13 16:48:25 -0500

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_185526) do
+ActiveRecord::Schema.define(version: 2021_08_30_023809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2021_07_22_185526) do
     t.integer "bike_count"
     t.string "fav_trail"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "user_id"
+    t.integer "zip"
+    t.string "content"
+    t.integer "likes"
+    t.integer "dislikes"
+    t.string "comments"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
